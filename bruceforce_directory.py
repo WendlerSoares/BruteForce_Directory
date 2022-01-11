c1aa35cd1a810558aa35f914b4027b91a8b572f7
Wendler <wendler-cleide@hotmail.com>
2022-01-11 15:48:33 -0300

    first commit

---
+++ bruceforce_directory.py
@@ -0,0 +1,15 @@
+import requests
+
+arquivo = open("listadiretorio.txt")
+
+linhas = arquivo.readlines()
+
+url = "http://g1.com.br/"
+
+for linha in linhas:
+    codigo = 404
+    if linha[0] != "#":
+        resp = requests.get(url+linha)
+        codigo = resp.status_code
+    if codigo != 404:
+        print(url+linha,codigo)
